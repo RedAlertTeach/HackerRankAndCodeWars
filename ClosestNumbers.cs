6d84a47a95217c270976ec9afdf3d385fe04fc56
Ms. Oakley <RedAlertTeach@users.noreply.github.com>
2019-06-10 13:59:55 -0400

    Add files via upload

---
+++ ClosestNumbers.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClosestNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  int[] numbers = new int[] { 3, 5, 2, 4, 1 };
+            int[] numbers = new int[] { -20, -3916237, -357920, -3620601, 7374819, -7330761, 30, 6246457, -6461594, 266854, -520, -470 };
+            int[] closest = FindClosestNumbers(numbers);
+            foreach (int n in closest)
+            {
+                Console.WriteLine("{0}", n);
+        
+            }
+            Console.ReadLine();
+        }
+        static public int[] FindClosestNumbers(int[] numbers)
+        {
+            Array.Sort(numbers);
+            int minDiff = int.MaxValue;
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                int diff = numbers[i] - numbers[i - 1];
+                if (diff < minDiff)
+                {
+                    minDiff = diff;
+                }
+            }
+            List<int> pairs = new List<int>();
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[i] - numbers[i-1] == minDiff)
+                {
+                    pairs.Add(numbers[i - 1]);
+                    pairs.Add(numbers[i]);
+                }
+            }
+            return pairs.ToArray();
+        }
+    }
+}
