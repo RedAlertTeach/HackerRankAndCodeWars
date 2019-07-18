e11bf362981b9d8cf61f57f55609b5f3c04d5b50
Ms. Oakley <RedAlertTeach@users.noreply.github.com>
2019-07-18 16:41:40 -0400

    Add files via upload

---
+++ PickingNumbers.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PIckingNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] { 1, 2, 2, 1, 2 };
+            int maxNum = LongestArray(numbers);
+            Console.WriteLine("The length of the maximum subarray is {0}", maxNum);
+            Console.ReadLine();
+        }
+        static int LongestArray(int[] numbers)
+        {
+            int[] counted = new int[100];
+            foreach (int i in numbers)
+            {
+                counted[i]++;
+            }
+            int largest = 0;
+            for (int i = 1; i < counted.Length; i++)
+            {
+                int difference = counted[i] + counted[i - 1];
+                if (difference > largest)
+                    largest = difference;
+            }
+            return largest;
+        }
+    }
+}
