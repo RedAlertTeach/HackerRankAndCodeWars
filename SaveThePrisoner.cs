4987800a17bd7248780b9d716767e2a10f4cf493
Ms. Oakley <RedAlertTeach@users.noreply.github.com>
2019-07-20 18:43:53 -0400

    Add files via upload

--- SaveThePrisoner.cs
+++ SaveThePrisoner.cs
@@ -1,23 +1,44 @@
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.IO;
+using System.Collections.Generic;
 using System.Linq;
-class Solution {
+using System.Text;
+using System.Threading.Tasks;
 
-    static int saveThePrisoner(int n, int m, int s){
-        // Complete this function
-        return (m % n) + s - 1;
-    }
-
-    static void Main(String[] args) {
-        int t = Convert.ToInt32(Console.ReadLine());
-        for(int a0 = 0; a0 < t; a0++){
-            string[] tokens_n = Console.ReadLine().Split(' ');
-            int n = Convert.ToInt32(tokens_n[0]);
-            int m = Convert.ToInt32(tokens_n[1]);
-            int s = Convert.ToInt32(tokens_n[2]);
-            int result = saveThePrisoner(n, m, s);
-            Console.WriteLine(result);
+namespace SaveThePrisoners
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = Environment.GetFolderPath(Environment.SpecialFolder.Personal) + "\\prisoners.txt";
+            string readText = File.ReadAllText(fileName);
+            string[] numberStrings = readText.Split(new char[] { ' ', '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            int[] numbers = Array.ConvertAll(numberStrings, int.Parse);
+            int number = numbers[0];
+            for (int i = 0; i < number; i++)
+            {
+                int n = numbers[3*i + 1];
+                int m = numbers[3*i + 2];
+                int s = numbers[3*i + 3];
+                Console.WriteLine("Test case number {0}: {1}", i + 1, Warn(n, m, s));
+            }
+            Console.ReadLine();
+        }
+        static int Warn(int n, int m, int s)
+        {
+            int result = 0;
+            if (m >= n)
+               result = (m % n) + s - 1;
+            else
+                result = s + m - 1;
+            if (result > n)
+                result = result - n;
+            else if (result == 0)
+                result = n;
+            return result;
         }
     }
+    
+
 }
