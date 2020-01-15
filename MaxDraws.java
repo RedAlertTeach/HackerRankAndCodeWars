825d5e072a57917715cef4f569e57c93d2f80382
Ms. Oakley <RedAlertTeach@users.noreply.github.com>
2020-01-15 13:51:15 -0500

    Add files via upload

---
+++ MaxDraws.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.math.*;
+import java.text.*;
+import java.util.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    /*
+     * Complete the maximumDraws function below.
+     */
+    static int maximumDraws(int n) {
+        /*
+         * Write your code here.
+         */
+         return n + 1;
+
+    }
