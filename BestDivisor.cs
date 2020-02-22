255c0c5bd9f15705b9feb5418c80ededeb1d7f45
Ms. Oakley <RedAlertTeach@users.noreply.github.com>
2020-02-22 14:23:53 -0500

    Add files via upload

--- BestDivisor.cs
+++ BestDivisor.cs
@@ -9,37 +9,42 @@ namespace BestDivisor
     class Program
     {
         static void Main(string[] args)
-
         {
-            int test = 12;
+            int test = 198;
             List<int> divisors = findDivisors(test);
             int best = findBest(divisors);
-            Console.WriteLine("The best divisor for {0} is {1}", test, best);
+            Console.WriteLine(best);
+          //  Console.WriteLine("The best divisor for {0} is {1}", test, best);
             Console.ReadLine();
         }
-
         static public List<int> findDivisors(int test)
         {
             List<int> results = new List<int>();
             results.Add(1);
-            for (int i = 2; i <= test/2; i++)
+            for (int i = 2; i <= test / 2; i++)
             {
                 if (test % i == 0)
                     results.Add(i);
             }
+            results.Add(test);
+
             return results;
         }
 
         static public int findBest(List<int> divisors)
         {
             int result = 1;
+            int best = 1;
             foreach (int d in divisors)
             {
                 int sum = findSum(d);
                 if (sum > result)
+                {
                     result = sum;
+                    best = d;
+                }
             }
-            return result;
+            return best;
         }
 
         static private int findSum(int number)
